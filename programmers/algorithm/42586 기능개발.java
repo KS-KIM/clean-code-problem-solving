730fdc4e7ed437e2fbffb0048dcc6c60c37d98e3
KSKIM <kwangsoonkim@outlook.com>
2019-12-12 01:01:24 +0900

    Fix typo

--- programmers/algorithm/42586 기능개발.java
+++ programmers/algorithm/42586 기능개발.java
@@ -6,26 +6,27 @@ import java.util.LinkedList;
 
 /**
  * See https://programmers.co.kr/learn/courses/30/lessons/42586
+ * 
  * @author KSKIM
  * @since 2019-12-12
  */
 class Work {
 	private static final int COMPLETE = 100;
-	
+
 	private int progress;
 	private final int speed;
-	
+
 	public Work(int progress, int speed) {
 		this.progress = progress;
 		this.speed = speed;
 	}
-	
+
 	public void work() {
 		if (!isFinished()) {
 			progress += speed;
 		}
 	}
-	
+
 	public boolean isFinished() {
 		return progress >= COMPLETE;
 	}
@@ -35,17 +36,17 @@ class ServiceDevelopment {
 	private static final int HAS_NO_WORK = 0;
 
 	private final List<Work> works;
-	
+
 	public ServiceDevelopment(List<Work> works) {
 		this.works = Objects.requireNonNull(works);
 	}
-	
+
 	public void work() {
-		for (Work work: works) {
+		for (Work work : works) {
 			work.work();
 		}
 	}
-	
+
 	public int distribute() {
 		int distributeCount = 0;
 		Iterator<Work> iter = works.iterator();
@@ -59,7 +60,7 @@ class ServiceDevelopment {
 		}
 		return distributeCount;
 	}
-	
+
 	public boolean hasWork() {
 		return works.size() > HAS_NO_WORK;
 	}
@@ -67,38 +68,38 @@ class ServiceDevelopment {
 
 class Solution {
 	private ServiceDevelopment serviceDevelopment;
-	
+
 	public int[] solution(int[] progresses, int[] speeds) {
 		init(progresses, speeds);
-        List<Integer> answer = new ArrayList<>();
-    	while (serviceDevelopment.hasWork()) {
-    		serviceDevelopment.work();
-    		int distributeCount = serviceDevelopment.distribute();
-    		if (distributeCount > 0) {
-    			answer.add(distributeCount);
-    		}
-    	}
-        return convertToArray(answer); 
-    }
-	
-	private void init(int[] progresses, int[] speeds) {
-    	List<Work> works = convertToWorks(progresses, speeds);
-    	serviceDevelopment = new ServiceDevelopment(works);
+		List<Integer> answer = new ArrayList<>();
+		while (serviceDevelopment.hasWork()) {
+			serviceDevelopment.work();
+			int distributeCount = serviceDevelopment.distribute();
+			if (distributeCount > 0) {
+				answer.add(distributeCount);
+			}
+		}
+		return convertToArray(answer);
 	}
-	
-    private List<Work> convertToWorks(int[] progresses, int[] speeds) {
-    	List<Work> works = new LinkedList<>();
-    	for (int index = 0; index < progresses.length; ++index) {
-    		works.add(new Work(progresses[index], speeds[index]));
-    	}
-    	return works;
-    }
-    
-    private int[] convertToArray(List<Integer> numbers) {
-    	int[] result = new int[numbers.size()];
-    	for (int index = 0; index < numbers.size(); ++index) {
-    		result[index] = numbers.get(index);
-    	}
-    	return result;
-    }
+
+	private void init(int[] progresses, int[] speeds) {
+		List<Work> works = convertToWorks(progresses, speeds);
+		serviceDevelopment = new ServiceDevelopment(works);
+	}
+
+	private List<Work> convertToWorks(int[] progresses, int[] speeds) {
+		List<Work> works = new LinkedList<>();
+		for (int index = 0; index < progresses.length; ++index) {
+			works.add(new Work(progresses[index], speeds[index]));
+		}
+		return works;
+	}
+
+	private int[] convertToArray(List<Integer> numbers) {
+		int[] result = new int[numbers.size()];
+		for (int index = 0; index < numbers.size(); ++index) {
+			result[index] = numbers.get(index);
+		}
+		return result;
+	}
 }
